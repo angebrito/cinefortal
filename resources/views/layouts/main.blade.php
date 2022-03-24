722d139ac3211a5ede5b8f87bcd29d9ad67de482
Angelica Brito <angelica.brito@outlook.com>
2022-03-24 12:12:32 -0300

    Movificação no nas informações do filme

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -4,9 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     
-    <title>Cinefortal</title>
-
-    <link rel="stylesheet" href={{asset("/css/app.css")}}>     
+    <title>Cinefortal</title>    
 
     <!--  Bootstrap  -->
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
