fb3b07de4d9579d22836f1b4b1817ff176f8573f
angebrito <49460211+angebrito@users.noreply.github.com>
2022-03-28 16:05:28 -0300

    Add files via upload
    Atualização de rotas

--- routes/web.php
+++ routes/web.php
@@ -2,9 +2,12 @@
 
 use App\Http\Controllers\LançamentosController;
 use App\Http\Controllers\MoviesController;
+use App\Http\Controllers\NowplayingController;
 use Illuminate\Support\Facades\Route;
 
 
 Route::get('/',[MoviesController::class, 'index']);
 Route::get('/movies/{movie}', [MoviesController::class, 'show']);
 
+Route::get('/nowplaying/{movie}', [NowplayingController::class, 'nowplaying']);
+Route::get('/top/{movie}', [NowplayingController::class, 'top']);
